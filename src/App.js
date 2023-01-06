6580886e6be1a3800bc9f203359a95240b4784aa
Spencer <spencerb242@proton.me>
2023-01-05 20:10:10 -0700

    added custom fetch hook and then used it to pull information from our json server into our home component. Mapped article title and author to be displayed on home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
@@ -10,9 +10,11 @@ function App() {
       <BrowserRouter>
         <nav>
           <h1>My Articles</h1>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
         </nav>
         <Switch>
           <Route exact path="/">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { useFetch } from "../hooks/useFetch";
 
 export default function Home() {
+  const {
+    data: articles,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/articles");
+
   return (
-    <div>
-      <h2>Homepage</h2>
-      <p>
-        Excepteur aute eu anim occaecat reprehenderit labore ipsum anim.
-        Exercitation mollit incididunt consequat ut enim cillum do non
-        reprehenderit quis culpa tempor. Mollit nulla voluptate mollit deserunt
-        officia consectetur sint proident commodo ad velit. Ex commodo aliqua in
-        labore et eiusmod ad voluptate ullamco ea do ut.
-      </p>
+    <div className="home">
+      <h2>Articles</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+          </div>
+        ))}
     </div>
   );
 }
